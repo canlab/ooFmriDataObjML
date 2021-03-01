e8d2fe425f228cdfff2148b61cf129344c3f32cb
bogpetre <bogpetre@gmail.com>
2021-03-01 15:06:12 -0700

    Bug fixes and additional features

--- estimators/classifiers/linearSvmClf.m
+++ estimators/classifiers/linearSvmClf.m
@@ -17,6 +17,9 @@
 %                   specified as fitclinearOpts argument. See help
 %                   fitclinearOpts for default value.
 %
+%   'regularization'
+%               - 'ridge', 'lasso', or 'none'
+%
 %   'fitclinearOpts'
 %               - cell array of options to pass through to firlinear. See
 %                   help fitrlinear for details. 
@@ -27,15 +30,12 @@
 %                   specify both Lambda in fitclinearOpts and C in the 
 %                   linearSvmClf constructor then a warning will be 
 %                   thrown and C will supercede Lambda.
-%                 Note: 'CVPartition' can take either cvpartition objects
-%                   or function handles to cvpartition generators that take
-%                   (X,Y) as input and return a cvpartition. e.g.
-%                   @(X,Y)cvpartition(ones(size(Y)),'KFOLD',5). 
-%                   This can be helpful for on demand cvpartition
-%                   generation, for instance if linearSvmClf is called by a
-%                   crossValidator object, but you want something like
-%                   lasso parameter estimation to be handled internally 
-%                   using (presumably) LARS.
+%                 Note: fitclinearOpts has the option of performing
+%                   hyperparameter optimization internally via kfold, hold
+%                   out or custom cvpartition specification. This is not
+%                   supported via linearSvmClf. Wrap linearSvmClf in a
+%                   hyperparameter optimization object like bayesOptCV
+%                   instead.
 %                   
 %
 %   Note: both lambda and C are hyperparameters, but you should NOT try to
@@ -58,21 +58,38 @@ classdef linearSvmClf < linearModelEstimator & modelClf
         scoreFcn;
     end
     
-    properties (Dependent, Access = ?modelEstimator)
+    properties (Access = private, Hidden)
+        % we need to store scoreFcn in both the fitclinear format (which is
+        % either a function handle or a string) and in a pure function
+        % handle format. To avoid confusion we keep the pure function
+        % handle format hidden from the user. This should never be accessed
+        % directly though. Always call by invoking scoreFcn, which will use
+        % it's get method to pull this data.
+        scoreFcn0 = @(x1)(x1);
+    end
+    
+    properties (Dependent, SetAccess = ?modelEstimator)
         lambda;
+        regularization;
     end  
     
     properties (SetAccess = private)                
         isFitted = false;
         fitTime = -1;
         
-        CV_funhan = [];
+        % CV_funhan = [];
         
-        decisionFcn = @(x1)(x1 > 0);
+        % this is a hacky way of implementing a ternary operator.
+        % 1     if x1 true
+        % -1    if x1 false
+        % I've made attempts to keep methods robust with respect to [0,1]
+        % or [-1,1] coding, but just in case we're going to try to use
+        % [-1,1] which is standard for SVMs.
+        decisionFcn = @(x1)subsref([-1; 1], substruct('()', {(x1 > 0) + 1}));
     end
     
     properties (Access = ?Estimator)
-        hyper_params = {'intercept', 'C', 'lambda'};
+        hyper_params = {'intercept', 'C', 'lambda', 'regularization'};
     end
           
     
@@ -81,6 +98,8 @@ classdef linearSvmClf < linearModelEstimator & modelClf
             % defaults
             scoreFcn = 'none';
             intercept = true;
+            learner = 'svm';
+            regularization = 'none';
             
             fitclinearOpts_idx = find(strcmp(varargin,'fitclinearOpts'));
             if ~isempty(fitclinearOpts_idx)
@@ -93,7 +112,17 @@ classdef linearSvmClf < linearModelEstimator & modelClf
                         case 'ScoreTransform'
                             scoreFcn = obj.fitclinearOpts{i + 1};
                         case 'FitBias'
-                            intercept = varargin{i+1};
+                            intercept = logical(varargin{i+1});
+                        case 'Learner'
+                            learner = varargin{i+1};
+                        case 'Regularization'
+                            regularization = varargin{i+1};
+                        case 'KFold'
+                            error('Internal cross validation is not supported. Please wrap linearSvmClf in a bayesOptCV object or similar instead');
+                        case 'CVPartition'
+                            error('Internal cross validation is not supported. Please wrap linearSvmClf in a bayesOptCV object or similar instead');
+                        case 'Holdout'
+                            error('Internal cross validation is not supported. Please wrap linearSvmClf in a bayesOptCV object or similar instead');
                     end
                 end
             end
@@ -103,6 +132,8 @@ classdef linearSvmClf < linearModelEstimator & modelClf
             % down the line. Better to have it in two separate invocations.
             obj.scoreFcn = scoreFcn;
             obj.intercept = intercept;
+            obj.learner = learner;
+            obj.regularization = regularization;
             
             % by parsing fitclinearOpts first we give override priority to
             % arguments passed directly to linearSvmClf, which we parse now
@@ -117,6 +148,10 @@ classdef linearSvmClf < linearModelEstimator & modelClf
                             if any(strcmp(obj.fitclinearOpts, 'Lambda'))
                                 warning('Will override fitclinearOpts Lambda = %0.3f, using C = %0.3f',obj.lambda, obj.C);
                             end
+                        case 'regularization'
+                            obj.regularization = varargin{i+1};
+                        case 'fitclinearOpts'
+                            continue;
                         otherwise
                             warning('Option %s not supported', varargin{i});
                     end
@@ -127,9 +162,8 @@ classdef linearSvmClf < linearModelEstimator & modelClf
                 obj.C = 1;
             end
             
-            obj.learner = 'svm';
-            
-            obj = obj.check_cv_params();
+            % see note above check_cv_params definition below
+            % obj = obj.check_cv_params();
         end
         
         function obj = fit(obj, X, Y)
@@ -137,9 +171,17 @@ classdef linearSvmClf < linearModelEstimator & modelClf
             assert(size(X,1) == length(Y), 'length(Y) ~= size(X, 1)');
             
             if ~isempty(obj.C) % if C parameter is in use, set lambda accordingly
-                obj.lambda = obj.C/length(Y);
+                % setting lambda erases C (since outside of this specific
+                % case, you can't reconcile the two, since you don't know
+                % n, and instead C is erased.
+                C = obj.C;
+                obj.lambda = C/length(Y);
+                obj.C = C;
             end
             
+            % see comment above check_cv_params() definition regarding this
+            % code.
+            %{
             % make a copy and convert cvpartition generator to cvpartition
             % class instance here (this does the actual partitioning). We
             % do this because we (a) don't want to directly overwrite
@@ -161,23 +203,28 @@ classdef linearSvmClf < linearModelEstimator & modelClf
             end
             
             Mdl = fitclinear(double(X),Y, fitclinearOpts{:});
+            %}
+            Mdl = fitclinear(double(X),Y, obj.fitclinearOpts{:});
+            
+            if isa(Mdl,'ClassificationPartitionedLinear')
+                error('linearSvmClf does not support using fitclinear''s internal cross validation. Please wrap linearSvmClf in a crossValScore() object instead.');
+            end
             
             obj.B = Mdl.Beta(:);
             obj.offset = Mdl.Bias;
             
+            obj.prior = sum(obj.decisionFcn(Y) == 1)/length(Y);
+            
             obj.isFitted = true;
             obj.fitTime = toc(t0);
         end
-        
+                
         function yfit_raw = score_samples(obj, X)
             yfit_raw = score_samples@linearModelEstimator(obj,X);
             yfit_raw = obj.scoreFcn(yfit_raw(:));
         end
         
-        function yfit = predict(obj, X)
-            yfit = obj.score_samples(X);
-            yfit = obj.decisionFcn(yfit);
-        end
+        %% methods for dependent properties
         
         function obj = set.learner(obj, val)
             if ~strcmp(val, 'svm')
@@ -196,7 +243,7 @@ classdef linearSvmClf < linearModelEstimator & modelClf
             learner_idx = find(strcmp(obj.fitclinearOpts, 'Learner'));
             assert(~isempty(learner_idx), 'Learner not specified in fitclinearOpts. This is strange.');
             
-            val = ob.fitclinearOpts{learner_idx+1};
+            val = obj.fitclinearOpts{learner_idx+1};
         end
         
         function obj = set.intercept(obj, val)
@@ -212,7 +259,7 @@ classdef linearSvmClf < linearModelEstimator & modelClf
             bias_idx = find(strcmp(obj.fitclinearOpts, 'FitBias'));
             assert(~isempty(bias_idx), 'FitBias not specified in fitclinearOpts. This is strange.');
             
-            val = ob.fitclinearOpts{bias_idx+1};
+            val = obj.fitclinearOpts{bias_idx+1};
         end
         
         function obj = set.lambda(obj, val)
@@ -224,6 +271,8 @@ classdef linearSvmClf < linearModelEstimator & modelClf
             else
                 obj.fitclinearOpts{lambda_idx + 1} = val;
             end
+            
+            obj.C = [];
         end
         
         function val = get.lambda(obj)            
@@ -235,36 +284,70 @@ classdef linearSvmClf < linearModelEstimator & modelClf
             end
         end
         
+        
+        function obj = set.regularization(obj, val)
+            % we cast to char() because bayesOpt will pass character
+            % vectors in as type categorical(), which will cause
+            % fitclinear to fail.
+            val = char(val);
+            assert(ismember(val,{'ridge','lasso','none'}), 'regularization must be ''ridge'', ''lasso'', or ''none''');
+            
+            regularization_idx = find(strcmp(obj.fitclinearOpts,'Regularization'));
+                
+            if strcmp(char(val),'none')
+                if isempty(regularization_idx)
+                    return
+                else
+                    obj.fitclinearOpts{regularization_idx:regularization_idx+1} = [];
+                end
+            else
+                if isempty(regularization_idx)
+                    obj.fitclinearOpts = [obj.fitclinearOpts, {'Regularization', val}];
+                else
+                    obj.fitclinearOpts{regularization_idx + 1} = val;
+                end
+            end
+        end
+        
+        function val = get.regularization(obj)            
+            regularization_idx = find(strcmp(obj.fitclinearOpts, 'Regularization'));
+            if ~any(regularization_idx)
+                val = [];
+            else
+                val = obj.fitclinearOpts{regularization_idx+1};
+            end
+        end
+        
         function obj = set.scoreFcn(obj, val)
             if ischar(val)
                 switch(val)
                     case 'doublelogit'
-                        obj.scoreFcn = @(x1)(1/(1 + exp(-2*x1)));
+                        obj.scoreFcn0 = @(x1)(1/(1 + exp(-2*x1)));
                     case 'invlogit'
-                        obj.scoreFcn = @(x1)(log(x1 / (1-x1)));
+                        obj.scoreFcn0 = @(x1)(log(x1 / (1-x1)));
                     case 'ismax'
-                        obj.scoreFcn = @(x1)(max(x1) == x1);
+                        obj.scoreFcn0 = @(x1)(max(x1) == x1);
                     case 'logit'
-                        obj.scoreFcn = @(x1)(1/(1+exp(-x1)));
+                        obj.scoreFcn0 = @(x1)(1/(1+exp(-x1)));
                     case 'none'
-                        obj.scoreFcn = @(x1)(x1);
+                        obj.scoreFcn0 = @(x1)(x1);
                     case 'identity'
-                        obj.scoreFcn = @(x1)(x1);
+                        obj.scoreFcn0 = @(x1)(x1);
                     case 'sign'
-                        obj.scoreFcn = @(x1)(sign(x1));
+                        obj.scoreFcn0 = @(x1)(sign(x1));
                     case 'symmetric'
-                        obj.scoreFcn = @(x1)(2*x1 - 1);
+                        obj.scoreFcn0 = @(x1)(2*x1 - 1);
                     case 'symetricismax'
-                        obj.scoreFcn = @symmetricismax;
+                        obj.scoreFcn0 = @symmetricismax;
                     case 'symetriclogit'
-                        obj.scoreFcn = @(x1)(2/(1 + exp(-x1)) - 1);
+                        obj.scoreFcn0 = @(x1)(2/(1 + exp(-x1)) - 1);
                     otherwise
                         error('scoreFcn %s not supported', val)
                 end
-            else
-                obj.scoreFcn = val;
-            end                    
-            assert(isa(val, 'function_handle'), 'val must be a function_handle or a supported ScoreTransform option for fitclinear');
+            else            
+                assert(isa(val, 'function_handle'), 'val must be a function_handle or a supported ScoreTransform option for fitclinear');
+                obj.scoreFcn0 = val;
+            end        
                                     
             % sync fitclinear args
             st_idx = find(strcmp(obj.fitclinearOpts, 'ScoreTransform'));
@@ -276,12 +359,18 @@ classdef linearSvmClf < linearModelEstimator & modelClf
         end
         
         function val = get.scoreFcn(obj)   
-            val = obj.scoreFcn;
+            val = obj.scoreFcn0;
         end
     end
     
     
     methods (Access = private)
+        % if you let fitclinear perform cross-val internally to optimize
+        % some parameter or other you will need to update fit to work on
+        % ClassificationPartitionedLinear objects that are the result of
+        % these CV routines, but it's not clear what purpose this would
+        % serve so for now this is commented out.
+        %{
         % fitclinear has an argument for specifying cross
         % validation folds, and this check incorporates a method for
         % allowing the user to specify function handles to cvpartition 
@@ -298,6 +387,7 @@ classdef linearSvmClf < linearModelEstimator & modelClf
                 end
             end
         end
+        %}
     end
     
     methods (Access = private, Static)
