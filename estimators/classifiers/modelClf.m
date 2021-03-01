b2532073cae2a8e932a4cb1cc276b416869b5ccc
bogpetre <bogpetre@gmail.com>
2021-02-28 21:34:10 -0700

    Draft upload, needs null predictor updates still

--- estimators/classifiers/modelClf.m
+++ estimators/classifiers/modelClf.m
@@ -1,16 +1,17 @@
-classdef (Abstract) linearModelClf < modelEstimator
-    properties (Abstract)
+classdef (Abstract) modelClf < modelEstimator
+    properties (Abstract)        
         decisionFcn; % used to convert raw scors to category labels
-    end
-    
-    methods (Abstract)
-        predict_null(obj, X, varargin);
     end
     
     methods
         function yfit = predict(obj, X, varargin)            
             yfit_raw = obj.score_samples(X, varargin{:});
             yfit = obj.decisionFcn(yfit_raw);
+        end
+
+        function yfit_null = predict_null(obj, varargin)
+            yfit_raw = obj.score_null(obj);
+            yfit_null = obj.decisionFcn(yfit_raw);
         end
     end
 end
