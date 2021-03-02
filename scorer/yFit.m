cebce3d7f9069df1b8a10eae7c7047616fb45467
bogpetre <bogpetre@gmail.com>
2021-03-01 17:32:06 -0700

    last commit before linearModelEstimator property update

--- scorer/yFit.m
+++ scorer/yFit.m
@@ -1,4 +1,8 @@
-% yFit objects are things scorers operate on
+% yFit objects are things scorers operate on. For classifier scorers may
+% either operate on labels (yfit) or on unlabeled outcome values
+% (yfit_raw), depending on the scorer. For instance hinge loss operates on
+% raw scores but f1 is computed based on labels. Regression scorers 
+% operate on yfit, but yfit and yfit_raw should be the same for regression.
 classdef (Abstract) yFit
     properties (SetAccess = protected)
         yfit = [];      % predicted scores or category labels
