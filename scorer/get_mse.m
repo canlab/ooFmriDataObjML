a50a886de746f6a47219359dd1d3c3f7022002e4
Bogdan Petre <f0042vm@g03.hpcc.dartmouth.edu>
2022-11-24 17:39:57 -0500

    More informative output for multivariate outcomes

--- scorer/get_mse.m
+++ scorer/get_mse.m
@@ -1,11 +1,17 @@
 function err = get_mse(yFitObj)
     assert(isa(yFitObj,'yFit'),'get_mse() takes only yFit objects as input');
 
+    %if isvector(yFitObj.Y)
+        meanFun = @(x1,x2)(mean((x1 - x2).^2));
+    %else
+    %    meanFun = @(x1,x2)(mean(arrayfun(@(ind)(mean((x1(:,ind) - x2(:,ind)).^2,1)),1:size(x2,2)),2));
+    %end
+    
     if ~isempty(yFitObj.yfit)
-        err = mean((yFitObj.yfit - yFitObj.Y).^2);
+        err = meanFun(yFitObj.yfit,yFitObj.Y);
     elseif ~isempty(yFitObj.yfit_raw)
         warning('yfit not found, using yfit_raw');
-        err = mean((yFitObj.yfit_raw - yFitObj.Y).^2);
+        err = meanFun(yFitObj.yfit_raw,yFitObj.Y);
     else
         error('Neither yfit nor yfit_raw found.');
     end
