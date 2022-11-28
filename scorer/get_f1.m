09fe156a1e28d00b17de3010888cb593b9a57db3
Bogdan Petre <f0042vm@n04.hpcc.dartmouth.edu>
2022-11-28 17:25:42 -0500

    more graceful handling of all labels being in one category

--- scorer/get_f1.m
+++ scorer/get_f1.m
@@ -2,12 +2,16 @@
 function err = get_f1(yFitObj)
     assert(isa(yFitObj,'yFit'),'get_f1() takes only yFit objects as input');
 
-    true_positive = (yFitObj.Y == 1).*(yFitObj.yfit == 1);
-    precision = sum(true_positive)/sum(yFitObj.yfit == 1);
-    
-    recall = sum(true_positive)/sum(yFitObj.Y == 1); % also known as the true positive rate
-    
-    f1 = 2 * precision * recall / (precision + recall);
+    if sum(yFitObj.yfit == 1) > 0
+        true_positive = (yFitObj.Y == 1).*(yFitObj.yfit == 1);
+        precision = sum(true_positive)/sum(yFitObj.yfit == 1);
+        
+        recall = sum(true_positive)/sum(yFitObj.Y == 1); % also known as the true positive rate
+        
+        f1 = 2 * precision * recall / (precision + recall);
+    else
+        warning('All labels assigned to a single class. Cannot compute precision, F1=0.');
+    end
     
     err = f1;
 end
